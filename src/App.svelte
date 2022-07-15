f23adb036fb8ed356bcd3e2919d820efb9267373
enzineer <sndy008@gmail.com>
2022-07-15 09:45:07 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="card">
+    <Counter />
+  </div>
+</main>
